881cc6adb0f01059144885c3bbe8bf62d8fba716
Artyom Abakumov <artem210966@gmail.com>
2024-05-01 18:20:09 +0300

    Cleanup project

--- crAll.bat
+++
@@ -1,3 +0,0 @@
-.\x64\Release\Barcode.exe 3 analysis\images\polus.png analysis\datasets
-.\x64\Release\Barcode.exe 3 analysis\images\winter2.png analysis\datasets
-.\x64\Release\Barcode.exe 3 analysis\images\correctpolus.png analysis\datasets
