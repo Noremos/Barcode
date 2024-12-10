b08e00a597bedfbdf4036024aa8caa737704bc17
Artyom Abakumov <artem210966@gmail.com>
2024-12-10 11:00:06 +0300

    Change library name and version

--- PrjBarlib/modules/python/BarcodeProject/test.sh
+++ PrjBarlib/modules/python/BarcodeProject/test.sh
@@ -1,6 +1,6 @@
 source ../.venv/bin/activate
 python -m build
 # stubgen -m libbarpy
-pip uninstall -y raster-barcode
+pip uninstall -y ImageTopoDec
 pip install dist/raster_barcode-1.0.0-py3-none-any.whl
 ls ../.venv/lib/python3.13/site-packages/raster_barcode
