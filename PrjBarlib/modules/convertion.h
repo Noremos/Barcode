8dfecc896352a45dc0af73eb6f95a79be0cd1020
Artyom Abakumov <artem210966@gmail.com>
2026-03-31 22:15:27 +0300

    Mark convert functions constexpr instead of static

--- PrjBarlib/modules/convertion.h
+++ PrjBarlib/modules/convertion.h
@@ -20,23 +20,23 @@ namespace
 {
 	constexpr uint8_t x = 0x0001;
 	constexpr uchar sysByteOredr = (std::endian::native == std::endian::little) ? 77 : 73;
-	uchar GLOBALloclByteOreder = (std::endian::native == std::endian::little) ? 77 : 73;
+	constexpr uchar GLOBALloclByteOreder = (std::endian::native == std::endian::little) ? 77 : 73;
 }
 struct increnenter
 {
-	increnenter(int st, int ed)
+	constexpr increnenter(int st, int ed)
 	{
 		rev = GLOBALloclByteOreder != sysByteOredr;
 		i = rev ? ed : st;
 	}
-	increnenter(int st, int ed, uchar imgByteOrder)
+	constexpr increnenter(int st, int ed, uchar imgByteOrder)
 	{
 		rev = imgByteOrder != sysByteOredr;
 		i = rev ? ed : st;
 	}
 	bool rev;
 	int i = 0;
-	int v()
+	constexpr int v()
 	{
 		return rev ? i-- : i++;
 	}
@@ -46,13 +46,13 @@ struct increnenter
 template<class T>
 struct increnenterBytes
 {
-	increnenterBytes(uchar *bytes, int st, int ed, uchar imgByteOrder)
+	constexpr increnenterBytes(uchar *bytes, int st, int ed, uchar imgByteOrder)
 	{
 		this->bytes = bytes;
 		rev = imgByteOrder != ::sysByteOredr;
 		i = rev ? ed : st;
 	}
-	increnenterBytes(uchar *bytes, int st, int ed)
+	constexpr increnenterBytes(uchar *bytes, int st, int ed)
 	{
 		this->bytes = bytes;
 		rev = ::GLOBALloclByteOreder != ::sysByteOredr;
@@ -61,7 +61,8 @@ struct increnenterBytes
 	uchar *bytes;
 	bool rev;
 	int i = 0;
-	T v(int off)
+
+	constexpr T v(int off)
 	{
 		return (T)bytes[(rev ? i-- : i++)] << off;
 	}
@@ -76,26 +77,26 @@ struct increnenterBytes
 //		std::reverse(istart, iend);
 //	}
 //}
-static ushort toShort(uchar *bytes)
+constexpr ushort toShort(uchar *bytes)
 {
 	increnenter t(0, 1);
 
 	return short((unsigned char)(bytes[t.v()]) << 8 | (bytes[t.v()]));
 }
-static uint toInt(uchar *bytes)
+constexpr uint toInt(uchar *bytes)
 {
 	//alternative int* y; y = reinterpret_cast<int*>(bytes); return *y;
 	increnenter t(0, 3);
 	return int((unsigned char)(bytes[t.v()]) << 24 | (bytes[t.v()]) << 16 | (bytes[t.v()]) << 8 | (bytes[t.v()]));
 }
-static float toFloat(uchar* bytes)
+constexpr float toFloat(uchar* bytes)
 {
 	tofloat conv;
 	memcpy(conv.data, bytes, 4);
 	//reorder(conv.data, 4);
 	return conv.value;
 }
-static double toDouble(uchar *bytes)
+constexpr double toDouble(uchar *bytes)
 {
 	toduble conv;
 	memcpy(conv.data, bytes, 8);
@@ -104,35 +105,18 @@ static double toDouble(uchar *bytes)
 }
 
 
-static long long toInt64(uchar *bytes)
+constexpr long long toInt64(uchar *bytes)
 {
 	//alternative int* y; y = reinterpret_cast<int*>(bytes); return *y;
 	increnenterBytes<long long> t(bytes, 0, 7);
 	return t.v(56) |t.v(48) |t.v(40) |t.v(32) |t.v(24) | t.v(16) | t.v(8) | t.v(0);
 }
 
-static void convert(uchar *bytes, float &out) { out = toFloat(bytes); }
-static void convert(uchar *bytes, int &out) { out = toInt(bytes);}
-static void convert(uchar* bytes, uchar& out)	{ out = bytes[0];}
-static void convert(uchar* bytes, double& out)	{out = toDouble(bytes);}
-static void convert(uchar* bytes, ushort& out)	{out = toShort(bytes);}
-static void convert(uchar* bytes, short& out)	{out = toShort(bytes);}
+constexpr void convert(uchar *bytes, float &out) { out = toFloat(bytes); }
+constexpr void convert(uchar *bytes, int &out) { out = toInt(bytes);}
+constexpr void convert(uchar* bytes, uchar& out)	{ out = bytes[0];}
+constexpr void convert(uchar* bytes, double& out)	{out = toDouble(bytes);}
+constexpr void convert(uchar* bytes, ushort& out)	{out = toShort(bytes);}
+constexpr void convert(uchar* bytes, short& out)	{out = toShort(bytes);}
 
-static void silense()
-{
-	uchar buff[16];
-	float a;
-	int b;
-	uchar c;
-	ushort d;
-	short e;
-	double f;
-	convert(buff, a);
-	convert(buff, b);
-	convert(buff, c);
-	convert(buff, d);
-	convert(buff, e);
-	convert(buff, f);
-	toInt64(buff);
-}
 #endif // CONVERTION_H
